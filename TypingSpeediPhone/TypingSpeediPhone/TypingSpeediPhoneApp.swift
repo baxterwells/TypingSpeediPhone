594b6f533ad7f413b56f07555a8cb1b30d46409c
bwells44 <baxtercwells@gmail.com>
2021-10-27 17:22:19 -0400

    Initial commit

---
+++ TypingSpeediPhone/TypingSpeediPhone/TypingSpeediPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypingSpeediPhoneApp.swift
+//  TypingSpeediPhone
+//
+//  Created by Baxter Wells on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct TypingSpeediPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
